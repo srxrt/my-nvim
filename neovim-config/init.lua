86529c4ab4a8f8ace7b94b7c18b945ab4be57dff
srxrt <str9703@gmail.com>
2025-04-09 01:57:19 +0900

    feat: add more plugin and optoins

--- neovim-config/init.lua
+++ neovim-config/init.lua
@@ -1,21 +1,6 @@
--- Set leader key
---
 vim.g.mapleader = " "
 
--- Basic settings
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.tabstop = 2 
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.wrap = false
-vim.opt.cursorline = true
-vim.opt.termguicolors = true
-vim.opt.clipboard = "unnamedplus"
-
--- Keymaps
-vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
-vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
-
 require("config.lazy")
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
--- neovim-config/lua/config/keymaps.lua
+++ neovim-config/lua/config/keymaps.lua
@@ -1,5 +1,33 @@
 vim.g.mapleader = " "
 
+-- Keybinding for showing the hover tooltip
+vim.api.nvim_set_keymap(
+	"n",
+	"gh",
+	":lua vim.lsp.buf.hover()<CR>",
+	{ desc = "Show tooltip", noremap = true, silent = true }
+)
+-- Set timeout for key sequences to 300ms
+vim.o.timeoutlen = 3000
+-- Bind `kk` to exit Insert mode
+vim.api.nvim_set_keymap("i", "kk", "<Esc>", { noremap = true, silent = true })
+--
+-- Map keys to create new lines above or below the cursor
+vim.api.nvim_set_keymap("n", "<leader>o", "o<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>O", "O<Esc>", { noremap = true, silent = true })
+
+-- LazyVim configuration example for opening terminal
+vim.api.nvim_set_keymap("n", "<leader>t", ":term<CR>", { noremap = true, silent = true })
+
+-- Map to toggle wrapping with <Leader>cw
+vim.api.nvim_set_keymap("n", "<Leader>cw", ":set wrap!<CR>", {
+	noremap = true,
+	silent = true,
+})
+
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
+
 vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>")
 vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>")
 vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>")
